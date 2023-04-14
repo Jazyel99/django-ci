c690e67f9c842cbdbc63bd49e21d6867014b72e2
Jazyel99 <jaziel@outlook.com>
2023-04-14 00:17:09 -0300

    feat(views): added projec_lsit view

--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from budget.models import Project
+
+def project_list(request):
+  project_list = Project.objects.all()
+  return render(request, 'budget/project-list.html', {'project_list': project_list})
